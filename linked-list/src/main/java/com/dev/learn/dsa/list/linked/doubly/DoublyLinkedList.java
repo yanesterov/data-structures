fdf82b085b92e85bbb16fcf67fcb5f1ca6bbc351
nesteroviury <nesteroviury@gmail.com>
2024-01-12 20:18:22 +0300

    [doubly linked list] insertion at the end of list implemented

--- linked-list/src/main/java/com/dev/learn/dsa/list/linked/doubly/DoublyLinkedList.java
+++ linked-list/src/main/java/com/dev/learn/dsa/list/linked/doubly/DoublyLinkedList.java
@@ -1,5 +1,6 @@
 package com.dev.learn.dsa.list.linked.doubly;
 
+import static java.lang.String.format;
 import static java.util.Objects.isNull;
 
 /**
@@ -18,16 +19,53 @@ public class DoublyLinkedList<T> {
      * @param item node value
      */
     public void push(T item) {
-        var newNode = new Node(item);
-        if (!isNull(head)) {
-            newNode.next = head;
-            head.prev = newNode;
+        var node = new Node(item);
+        if (isNull(head)) {
+            tail = node;
+        } else {
+            node.next = head;
+            head.prev = node;
         }
-        head = newNode;
+        head = node;
     }
 
-    public T getHeadNodeValue() {
+    /**
+     * Add a node at the end of the list
+     *
+     * @param item node value
+     */
+    public void enqueue(T item) {
+        var prevTail = tail;
+        tail = new Node(item);
+        if (isNull(head)) {
+            head = tail;
+        } else {
+            prevTail.next = tail;
+        }
+    }
+
+    public T getHeadValue() {
         return !isNull(head) ? head.item : null;
+    }
+
+    public T getTailValue() {
+        return !isNull(tail) ? tail.item : null;
+    }
+
+    @Override
+    public String toString() {
+        var stringBuilder = new StringBuilder("linked list: ");
+        if (isNull(head)) {
+            stringBuilder.append("empty");
+        } else {
+            var node = head;
+            while (!isNull(node)) {
+                stringBuilder.append(format("[%s] -> ", node.item));
+                node = node.next;
+            }
+        }
+        var result = stringBuilder.toString();
+        return result.substring(0, result.length() - 3);
     }
 
     private class Node {
--- linked-list/src/test/java/com/dev/learn/dsa/list/linked/doubly/DoublyLinkedListTest.java
+++ linked-list/src/test/java/com/dev/learn/dsa/list/linked/doubly/DoublyLinkedListTest.java
@@ -1,41 +1,90 @@
 package com.dev.learn.dsa.list.linked.doubly;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class DoublyLinkedListTest {
 
-    @Test
-    @DisplayName("should push() add new node at the front of empty list")
-    void shouldAddNodeAtFrontOfEmptyList() {
-        //given
-        var list = new DoublyLinkedList<String>();
-        assertThat(list)
-                .hasFieldOrPropertyWithValue("head", null)
-                .hasFieldOrPropertyWithValue("tail", null);
-        var nodeValue = "item";
-        //when
-        list.push(nodeValue);
-        //then
-        assertThat(list).hasAllNullFieldsOrPropertiesExcept("head");
-        assertThat(list.getHeadNodeValue()).isEqualTo(nodeValue);
-    }
+    @Nested
+    @DisplayName("insertion node into list")
+    class Insertion {
 
-    @Test
-    @DisplayName("should push() add new node at the front of list")
-    void shouldAddNodeAtFrontOfList() {
-        //given
-        var list = new DoublyLinkedList<String>();
-        var firstValue = "firstValue";
-        var secondValue = "secondValue";
-        list.push(firstValue);
-        //when
-        list.push(secondValue);
-        //then
-        assertThat(list).hasAllNullFieldsOrPropertiesExcept("head");
-        list.getHeadNodeValue();
-        assertThat(list.getHeadNodeValue()).isEqualTo(secondValue);
+        @Test
+        @DisplayName("should push() add new node at the front of empty list")
+        void shouldAddNodeAtFrontOfEmptyList() {
+            //given
+            var list = new DoublyLinkedList<String>();
+            assertThat(list)
+                    .hasFieldOrPropertyWithValue("head", null)
+                    .hasFieldOrPropertyWithValue("tail", null);
+            var nodeValue = "item";
+            //when
+            list.push(nodeValue);
+            //then
+            assertThat(list).hasNoNullFieldsOrProperties();
+            assertThat(list.getHeadValue()).isEqualTo(nodeValue);
+            assertThat(list.getTailValue()).isEqualTo(nodeValue);
+            System.out.printf("[AddNodeAtFrontOfEmptyList] %s%n", list);
+        }
+
+        @Test
+        @DisplayName("should push() add new node at the front of list")
+        void shouldAddNodeAtFrontOfList() {
+            //given
+            var list = new DoublyLinkedList<String>();
+            var headValue = "6";
+            var tailValue = "0";
+            //when
+            IntStream.range(0, 7).mapToObj(String::valueOf).forEach(list::push);
+            //then
+            assertThat(list).hasNoNullFieldsOrProperties();
+            list.getHeadValue();
+            assertThat(list.getHeadValue()).isEqualTo(headValue);
+            assertThat(list.getTailValue()).isEqualTo(tailValue);
+            System.out.printf("[AddNodeAtFrontOfList] %s%n", list);
+        }
+
+        @Test
+        @DisplayName("should enqueue() add new node at the end of empty list")
+        void shouldAddNodeAtEndOfEmptyList() {
+            //given
+            var list = new DoublyLinkedList<Integer>();
+            assertThat(list)
+                    .hasFieldOrPropertyWithValue("head", null)
+                    .hasFieldOrPropertyWithValue("tail", null);
+            var nodeValue = 0;
+            //when
+            list.push(nodeValue);
+            //then
+            assertThat(list).hasNoNullFieldsOrProperties();
+            assertThat(list.getHeadValue()).isEqualTo(nodeValue);
+            assertThat(list.getTailValue()).isEqualTo(nodeValue);
+            System.out.printf("[AddNodeAtEndOfEmptyList] %s%n", list);
+        }
+
+        @Test
+        @DisplayName("should enqueue() add new node at the end of list")
+        void shouldAddNodeAtEndOfList() {
+            //given
+            var list = new DoublyLinkedList<Integer>();
+            assertThat(list)
+                    .hasFieldOrPropertyWithValue("head", null)
+                    .hasFieldOrPropertyWithValue("tail", null);
+            var headValue = 0;
+            var tailValue = 6;
+            //when
+            IntStream.range(0, 7).forEach(list::enqueue);
+            //then
+            assertThat(list).hasNoNullFieldsOrProperties();
+            list.getHeadValue();
+            assertThat(list.getHeadValue()).isEqualTo(headValue);
+            assertThat(list.getTailValue()).isEqualTo(tailValue);
+            System.out.printf("[AddNodeAtEndOfList] %s%n", list);
+        }
     }
 }
